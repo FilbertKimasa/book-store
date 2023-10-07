b00465f841c6748f2843dc8b66831c153be52156
FilbertKimasa <filbertdan67@gmail.com>
2023-10-07 14:47:27 +0300

    Add BookImput component

---
+++ src/components/ImputBook.js
@@ -0,0 +1,32 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+
+function ImputBook() {
+  return (
+    <>
+      <p>ADD NEW BOOK</p>
+      <form>
+        <input
+          type="text"
+          placeholder="Book title"
+          className="input-text"
+          required
+        />
+        <select name="drinks" required>
+          <option value="" disabled selected hidden>
+            Category
+          </option>
+          <option value="tea">Finction</option>
+          <option value="milk">Biography</option>
+          <option value="milk">Academic</option>
+          <option value="milk">Wealth</option>
+        </select>
+        <button type="submit" className="input-submit">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default ImputBook;
