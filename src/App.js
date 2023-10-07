14dfbb869d398accb316b2b06cf21f4dea946b79
FilbertKimasa <filbertdan67@gmail.com>
2023-10-07 14:50:12 +0300

    install routing dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import BooksLogic from './components/BooksLogic';
+import Navigation from './components/Navigation';
+import BookCategory from './components/BookCategory';
+
 function App() {
-  return <div>Hi</div>;
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<BooksLogic />} />
+        <Route path="/books" element={<BooksLogic />} />
+        <Route path="/category" element={<BookCategory />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
