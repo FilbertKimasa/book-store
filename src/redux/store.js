c9bfa23fa36bdeb898cc89a36625074cf0849678
FilbertKimasa <filbertdan67@gmail.com>
2023-10-11 17:04:43 +0300

    Add store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import bookReducer from 'booksSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    books: bookReducer,
+  },
 });
 
 export default store;
