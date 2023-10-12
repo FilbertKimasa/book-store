b3d2569fb5d92e4b811cac9390853b46ac0469f4
FilbertKimasa <filbertdan67@gmail.com>
2023-10-12 08:18:37 +0300

    Add categoryReducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from 'booksSlice';
+import categoryReducer from 'categorySclcie';
 
 const store = configureStore({
   reducer: {
     books: bookReducer,
+    categories: categoryReducer,
   },
 });
 
