7c8e7d10e97ef27bcae1824cd22222090d3d9b7e
FilbertKimasa <filbertdan67@gmail.com>
2023-10-12 16:16:01 +0300

    Modify return

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import BookItem from './BookItem';
 
-function BookList({ booksProps }) {
+function BookList() {
+  const book = useSelector((state) => state.books.books);
   return (
     <ul>
-      {booksProps.map((book) => (
+      {book.map((book) => (
         <BookItem key={book.id} itemProp={book} />
       ))}
     </ul>
   );
 }
-BookList.propTypes = {
-  booksProps: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
-};
 
 export default BookList;
