fbb0a00c2469f0fdad66804967c89dadc3db584d
FilbertKimasa <filbertdan67@gmail.com>
2023-10-12 16:18:37 +0300

    Modify return

--- src/components/BooksLogic.js
+++ src/components/BooksLogic.js
@@ -2,23 +2,9 @@ import BookList from './BookList';
 import ImputBook from './ImputBook';
 
 function BooksLogic() {
-  const books = [
-    {
-      id: 1,
-      title: 'Things Fall Apart',
-    },
-    {
-      id: 2,
-      title: 'Think Like a Man',
-    },
-    {
-      id: 3,
-      title: 'C++ Primer 5th Edition',
-    },
-  ];
   return (
     <div>
-      <BookList booksProps={books} />
+      <BookList />
       <ImputBook />
     </div>
   );
