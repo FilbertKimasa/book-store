41b53dc4eb95e60bb5fc7125396226c30cf0cb37
FilbertKimasa <filbertdan67@gmail.com>
2023-10-12 16:26:55 +0300

    Import reducers to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import bookReducer from 'booksSlice';
-import categoryReducer from 'categorySclcie';
+import bookReducer from './books/booksSlice';
+import categoryReducer from './cetgories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
