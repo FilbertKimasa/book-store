5e3dc4b7cec4f25d864c6391f16e1decbf0fa8d6
FilbertKimasa <filbertdan67@gmail.com>
2023-10-12 16:25:53 +0300

    Update categories state

--- src/redux/cetgories/categoriesSlice.js
+++ src/redux/cetgories/categoriesSlice.js
@@ -1,19 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  categories: [],
+  categories: ['Under construction'],
 };
 
 const categorySlice = createSlice({
   name: 'categories',
   initialState,
-  reducers: {
-    getStatus: (state) => {
-      state.categories = 'Under construction';
-    },
-  },
 });
-
-export const { getStatus } = categorySlice.actions;
 
 export default categorySlice.reducer;
