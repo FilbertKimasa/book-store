1dc82177b0bb4205153f856363f08db8d4ff2921
FilbertKimasa <filbertdan67@gmail.com>
2023-10-14 18:28:05 +0300

    Receive and structure data received from API

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import BookItem from './BookItem';
+import { fetchBooks } from '../redux/books/booksSlice';
 
 function BookList() {
+  const dispatch = useDispatch();
   const book = useSelector((state) => state.books.books);
-  return (
-    <ul>
-      {book.map((book) => (
-        <BookItem key={book.item_id} itemProp={book} />
-      ))}
-    </ul>
-  );
+  const availableBooks = Object.keys(book).map((key) => ({
+    ...book[key][0],
+    item_id: key,
+  }));
+  const bookList = availableBooks.map((book) => (
+    <BookItem key={book.item_id} itemProp={book} />
+  ));
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
+  return <ul>{bookList}</ul>;
 }
 
 export default BookList;
